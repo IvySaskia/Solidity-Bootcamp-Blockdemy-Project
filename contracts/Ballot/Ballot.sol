e8128f8ef3391954a3ef90c6f097db840efa22fc
Remix Workspace <>
2022-10-04 20:19:47 -0400

    UPDATE: Refactor functions

--- contracts/Ballot/Ballot.sol
+++ contracts/Ballot/Ballot.sol
@@ -4,30 +4,32 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract MyBallot {
 
+    // STORAGE VARIABLES
+
     struct Voter {
         bool canVote;
         bool hasVoted;
     }
 
     struct Candidate {
-        string name;
-        string party;
+        string nameProject;
         uint votesCount;
     }
 
-    address public contractOwner;
+    address private contractOwner;
 
-    mapping(address => Voter) public voters;
+    mapping(address => Voter) private voters;
 
-    Candidate[] public candidates;
+    Candidate[] private candidates;
 
     uint private totalVotesCounter;
 
     uint private maxVotes;
 
+
     // CONSTRUCTOR
 
-    constructor(Candidate[] memory _candidates, uint _maxVotes) {
+    constructor(string[] memory _candidates, uint _maxVotes) {
         contractOwner = msg.sender;
         setCandidatesIntoStorage(_candidates);        
         maxVotes = _maxVotes;
@@ -45,6 +47,7 @@ contract MyBallot {
     }
 
     modifier giveRightToVoteModifier(address voterAddress) {
+        getTotalVotesCounterReachedRequire();
         require(
             areYouContractOwner(),
             "Only the contract owner can give right to vote."
@@ -54,10 +57,7 @@ contract MyBallot {
     }
 
     modifier voteModifier(address voterAddress) {
-        require(
-            totalVotesCounter < maxVotes, 
-            "Max account votes is reached"
-        );
+        getTotalVotesCounterReachedRequire();
         require(
             areYouHaveRigthToVote(), 
             "You have not right to vote"
@@ -69,16 +69,6 @@ contract MyBallot {
 
     // REQUIRES
 
-    function setCandidatesIntoStorage(Candidate[] memory _candidates) private {
-        for (uint index = 0; index < _candidates.length; index++) {
-            candidates.push(Candidate({
-                name: _candidates[index].name,
-                party: _candidates[index].party,
-                votesCount: 0
-            }));
-        }
-    }
-
     function getVoterVotedRequire(address voterAddress) private view {
         return require(
             !getVoterVoted(voterAddress),
@@ -86,18 +76,34 @@ contract MyBallot {
         );
     }
 
+    function getTotalVotesCounterReachedRequire() private view {
+        return require(
+            totalVotesCounter < maxVotes, 
+            "Max account votes is reached. Ballot is CLOSED."
+        );
+    }
+
 
     // FUNCTIONS
+
+    function setCandidatesIntoStorage(string[] memory _candidates) private {
+        for (uint index = 0; index < _candidates.length; index++) {
+            candidates.push(Candidate({
+                nameProject: _candidates[index],
+                votesCount: 0
+            }));
+        }
+    }
     
     function giveRightToVote(address voterAddress) public giveRightToVoteModifier(voterAddress) {
         voters[voterAddress].canVote = true;
     }
 
-    function vote(string memory name) public voteModifier(msg.sender) {
+    function vote(string memory _nameProject) public voteModifier(msg.sender) {
         Voter storage sender = voters[msg.sender]; // this is to show the usage of storage | I could use directly voters[msg.sender].hasVoted
         
         for (uint index = 0; index < candidates.length; index++) {
-            if (stringEqualTo(candidates[index].name,name)) {
+            if (stringEqualTo(candidates[index].nameProject, _nameProject)) {
                 candidates[index].votesCount++;
                 sender.hasVoted = true;
                 totalVotesCounter++;
@@ -105,16 +111,17 @@ contract MyBallot {
         }
     }
 
-    function getWinnerName() public view returns (string memory winnerName) {
+    function getWinnerName() public view returns (string memory winnerName, address) {
         (uint winningProposal, bool isTie) = getWinningCandidate();
         require(
             !isTie,
-            "There is a tie on Ballot"
+            "There is a tie on Ballot. You should start a new ballot with tie candidates."
         );
-        winnerName = candidates[winningProposal].name;
+        winnerName = candidates[winningProposal].nameProject;
+        return (winnerName, getContractAddress());
     }
 
-    function getWinningCandidate() public view returns (uint winningProposal, bool isTie) {        
+    function getWinningCandidate() private view returns (uint winningProposal, bool isTie) {        
         uint winningVoteCount;
 
         for (uint indexCandidate = 0; indexCandidate < candidates.length; indexCandidate++) {
@@ -128,38 +135,45 @@ contract MyBallot {
         }
     }
 
-    function getCandidatesList() public view returns (Candidate[] memory) {
-        return candidates;
+    function getCandidatesListName() public view returns (string[] memory) {
+        uint candidatesLength = candidates.length;
+        string[]  memory candidatesListName = new string[](candidatesLength);
+        
+        for (uint index = 0; index < candidatesLength; index++) {
+            candidatesListName[index] = candidates[index].nameProject;
+        }
+
+        return candidatesListName;
     }
 
-    function getContractAddress() public view returns (address) {
+    function getContractAddress() private view returns (address) {
         return address(this);
     }
 
-    function areYouContractOwner() public view returns (bool) {
+    function areYouContractOwner() private view returns (bool) {
         return msg.sender == contractOwner;
     }
 
-    function areYouHaveRigthToVote() public view returns (bool) {
+    function areYouHaveRigthToVote() private view returns (bool) {
         return getVoterRigthToVote(msg.sender);
     }
 
-    function getVoterRigthToVote(address voterAddress) public view returns (bool) {
+    function getVoterRigthToVote(address voterAddress) private view returns (bool) {
         return voters[voterAddress].canVote;
     }
 
-    function areYouVoted() public view returns (bool) {
+    function areYouVoted() private view returns (bool) {
         return getVoterVoted(msg.sender);
     }
 
-    function getVoterVoted(address voterAddress) public view returns (bool) {
+    function getVoterVoted(address voterAddress) private view returns (bool) {
         return voters[voterAddress].hasVoted;
     }
 
 
     // OTHER FUNCTIONS
 
-    function stringEqualTo(string memory s1, string memory s2) internal pure returns (bool) {
+    function stringEqualTo(string memory s1, string memory s2) private pure returns (bool) {
         return keccak256(abi.encode(s1)) == keccak256(abi.encode(s2));
     }
 }
